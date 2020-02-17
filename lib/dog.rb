f29d9553fc3b3ebe1138556698ea37ab525b6111
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-17 18:41:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def print_all
+    @@all.each do |name|
+      puts name 
+    end 
+  end 
+  
+  def clear_all 
+    @@all.clear
+  end 
+  
+  def save 
+  end 
+  
+  attr_accessor :name 
+end 
